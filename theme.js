ea9a234146c33068af82a1fe09c3c975205f49ca
itsnilskerwer <uougk@student.kit.edu>
2025-06-04 16:45:32 +0200

    Add missing JS

--- theme.js
+++ theme.js
@@ -1,9 +1,13 @@
-themeToggle.addEventListener("click", () => {
-  if (document.documentElement.classList.contains("light-mode")) {
-    document.documentElement.classList.add("dark-mode");
-    document.documentElement.classList.remove("light-mode");
-  } else {
-    document.documentElement.classList.add("light-mode");
-    document.documentElement.classList.remove("dark-mode");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggle = document.getElementById("themeToggle");
+
+  themeToggle.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("light-mode")) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  });
 });
